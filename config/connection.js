dc62dd59df8977c7cc07a962ae11a00cda8e7cce
Stephen <stephencooper11395@gmail.com>
2024-07-24 16:41:39 -0500

    edit connection pt 2

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,17 @@
-require('dotenv').config(); // Make sure this is at the top
+require('dotenv').config(); 
 
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true, 
+      rejectUnauthorized: false 
+    }
+  },
   logging: false,
 });
 
 module.exports = sequelize;
+
