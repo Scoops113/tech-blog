8f9585373c6978edd81ced6bdcf4040523ae030e
Stephen <stephencooper11395@gmail.com>
2024-07-24 16:38:20 -0500

    edited connection

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,10 @@
+require('dotenv').config(); // Make sure this is at the top
+
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('tech_blog_db', 'Stephen C', 'password', {
-  host: 'localhost',
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   logging: false,
 });
 
 module.exports = sequelize;
-
